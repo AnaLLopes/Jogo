728cf30b3c78ffcdb7df23d65c1e188fbe64cb48
Fernanda Farias <143156498+FeFeFarias05@users.noreply.github.com>
2023-12-04 11:24:52 -0300

    já com o metodo compare

---
+++ tf-jogo/tf-jogo/BuracoNegro.java
@@ -0,0 +1,38 @@
+import java.awt.Color;
+public class BuracoNegro implements ElementoMapa {
+    private Color cor;
+    private Character simbolo;
+
+    public BuracoNegro(Character simbolo, Color cor) {
+        this.simbolo = simbolo;
+        this.cor = cor;
+    }
+    
+    public Character getSimbolo() {
+        return simbolo;
+    }
+
+    public Color getCor() {
+        return cor;
+    }
+
+    @Override
+    public boolean podeSerAtravessado() {
+        return true;
+    }
+
+    @Override
+    public boolean podeInteragir() {
+        return true;
+    }
+
+    @Override
+    public String interage() {
+        return "AVISO!!! Não caia se não irá morrer e perder o jogo";
+    }
+
+    
+}
+
+    
+
